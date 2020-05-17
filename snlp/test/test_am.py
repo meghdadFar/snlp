46ce42c3e9fd10fef207a0887f6f7ce95320b287
meghdadFar <meghdad.farahmand@gmail.com>
2020-05-17 12:12:36 +0200

    Add unittest

---
+++ snlp/test/test_am.py
@@ -0,0 +1,19 @@
+import unittest
+from snlp.mwes.am import extract_ncs_from_sent, get_ams, get_counts
+
+
+class TestAms(unittest.TestCase):
+
+    def test_extract_nsc(self):
+        self.assertRaises(TypeError, extract_ncs_from_sent, 5)
+        self.assertRaises(TypeError, extract_ncs_from_sent, -2)
+        self.assertRaises(TypeError, extract_ncs_from_sent, [1, 2, 3])
+        self.assertRaises(TypeError, extract_ncs_from_sent, [1, 2, 'test'])
+        
+        self.assertRaises(ValueError, extract_ncs_from_sent, '')
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
