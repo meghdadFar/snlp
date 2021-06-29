50a1abd8b3670eb820143be434b61e579626db85
meghdadFar <meghdad.farahmand@gmail.com>
2021-06-29 20:47:22 +0200

    Refactor

--- snlp/preprocessing/filtering.py
+++ snlp/preprocessing/filtering.py
@@ -1,111 +1,104 @@
 import os
-
 import pandas as pd
-
-from matplotlib import pyplot as plt
 from scipy.stats import zscore
 from snlp import logger
 from snlp import gaussianize
 from sklearn.feature_extraction.text import TfidfVectorizer
+from typing import Set
 
 
-class WordFilter(object):
-    def __init__(self):
+class RedunWords(object):
+    """"Class to identify and represent a set of redundant words."""
+    def __init__(self, documents):
+        self.documents = documents
         pass
 
-    def analysis_report(self, documents):
-        """Plot word frequency needed for different types of analysis.
-
-        """
-        vectorizer = TfidfVectorizer(min_df=1)
-        X = vectorizer.fit_transform(documents)
-
-    def idf_filterset(self, documents, method="automatic", z=None, l_idf=1, u_idf=8):
-        """Creates a filter set by identifying words with anomalous IDF value.
+    def get_redundant_terms(self, method="idf", z=3, manual=False, manual_thresholds: dict={'lower_threshold':1, 'upper_threshold': 8}):
+        """Create a filter set by identifying words with anomalous statistics.
 
         Args:
-            documents (iterable): An iterable which yields either str, unicode or file objects.
-            method (string): Method of creating filterset: automatic or manual.
-            z (float): Z-score of the IDF distribution above which words are considered anomalous.
-            l_idf  (float): Lower cut-off threshold for IDF (inclusive). Used only when 
-            u_idf (float): Upper cut-off threshold for IDF (inclusive).
+            documents: An iterable which yields either str, unicode or file objects.
+            method: Method of creating the set of redundant words. Can be either of ['idf'].
+            z: Words with a Z-score above this value are considered redundant. Default is 3.
+            l_idf: Lower cut-off threshold for IDF (inclusive). Used only when 
+            u_idf: Upper cut-off threshold for IDF (inclusive).
 
         Returns:
-            filter_set (set): Set of filter words
+            filter_set (set): Set of filter words.
         """
+        token_score_dict = {}
+        if method == 'idf':
+            vectorizer = TfidfVectorizer(min_df=1)
+            X = vectorizer.fit_transform(self.documents)
+            idf = vectorizer.idf_
+            token_score_dict = dict(zip(vectorizer.get_feature_names(), idf))
+        else:
+            raise ValueError(f'Currently, the only available method is idf but you have specified {method}')
 
-        vectorizer = TfidfVectorizer(min_df=1)
-        X = vectorizer.fit_transform(documents)
+        if not manual:
+            redundant_word_set = self._redundant_terms_zscore(token_score_dict=token_score_dict, zscore=z)
+        else:
+            redundant_word_set = self._redundant_terms(token_score_dict=token_score_dict,
+                                            lower_threshold=manual_thresholds['lower_threshold'],
+                                            upper_threshold=manual_thresholds['upper_threshold'])
+        return redundant_word_set
 
-        if method == "automatic":
-            if z:
-                filterset = self._create_filter_zscore(vectorizer, zscore_threshold=z)
-            else:
-                filterset = self._create_filter_zscore(vectorizer)
-        elif method == "manual":
-            filterset = self._create_filter(vectorizer, lower_idf=l_idf, upper_idf=u_idf)
-
-        return filterset
-
-    def _create_filter(self, vectorizer, lower_idf, upper_idf):
-        """
-
+    def _redundant_terms(self, token_score_dict, lower_threshold, upper_threshold) -> Set[str]:
+        """Identify redundant terms by looking at manual thresholds i.e. lower_threshold, upper_threshold. 
+        
         Args:
-            lower_idf  (float): Lower cut-off threshold for IDF (inclusive).
-            upper_idf (float): Upper cut-off threshold for IDF (inclusive).
+            token_metric_dict: Dictionary of tokens to their metric.
+            lower_threshold: Lower cut-off threshold (inclusive).
+            upper_threshold: Upper cut-off threshold (inclusive).
 
+        Returns:
+            redundant_terms: Set of redundant terms.
         """
-
-        idf = vectorizer.idf_
-        tfidf = dict(zip(vectorizer.get_feature_names(), idf))
-        sl = sorted(tfidf.items(), key=lambda kv: kv[1])
-
+        sl = sorted(token_score_dict.items(), key=lambda kv: kv[1])
         smallest_idf = sl[0][1]
         largest_idf = sl[len(sl) - 1][1]
-
-        if lower_idf < smallest_idf:
+        if lower_threshold < smallest_idf:
             raise ValueError(
-                "Idf values are between [%.2f, %.2f]. You have set lower_idf to %.2f. Update the values accordingly, \
-                or consider continuing with automatic filter creation."
-                % (smallest_idf, largest_idf, lower_idf)
+                f'Idf values are between {smallest_idf:.2f} and {largest_idf:.2f}. You have set the lower_idf to {lower_threshold:.2f}. Update the values accordingly, \
+                or consider switching the manual flag back to False.'
             )
-
-        if upper_idf > largest_idf:
+        if upper_threshold > largest_idf:
             raise ValueError(
-                "Idf values are between [%.2f, %.2f]. You have set upper_idf to %.2f. Update the values accordingly, \
-                or consider using automatic filter creation."
-                % (smallest_idf, largest_idf, upper_idf)
+                f'Idf values are between {smallest_idf:.2f} and {largest_idf:.2f}. You have set the upper_idf to {upper_threshold:.2f}. Update the values accordingly, \
+                or consider switching the manual flag back to False.'
             )
-
-        filter_words = set()
+        redundant_terms = set()
         for i in range(len(sl)):
-            if sl[i][1] < lower_idf:
-                filter_words.add(sl[i][0])
-            if sl[i][1] >= upper_idf:
-                filter_words.add(sl[i][0])
+            if sl[i][1] < lower_threshold:
+                redundant_terms.add(sl[i][0])
+            if sl[i][1] >= upper_threshold:
+                redundant_terms.add(sl[i][0])
+        return redundant_terms
 
-        return filter_words
+    def _redundant_terms_zscore(self, token_score_dict, zscore):
+        """Identify redundant terms by looking at the provided Z-score.
 
-    def _create_filter_zscore(self, vectorizer, zscore_threshold=3):
-        idf = vectorizer.idf_
-        token_idp_dict = dict(zip(vectorizer.get_feature_names(), idf))
-        idf_df = pd.DataFrame(token_idp_dict.items(), columns=["token", "idf"])
+        Args:
+            token_metric_dict: Dictionary of tokens to their metric.
+            zscore: Words with a Z-score above this value are considered redundant.
 
+        Returns:
+            redundant_terms: Set of redundant terms.
+        """
+        idf_df = pd.DataFrame(token_score_dict.items(), columns=["token", "idf"])
         # Gaussianize idf
         g = gaussianize.Gaussianize(strategy="brute")
         g.fit(idf_df["idf"])
         idf_guassian = g.transform(idf_df["idf"])
         idf_df["idf_gaussianized"] = idf_guassian
-
         # Calculate z score
         z = zscore(idf_df["idf_gaussianized"])
         idf_df["zscore"] = z
-
-        filter_set = set()
+        redundant_terms = set()
         for i in range(len(idf_df)):
-            if idf_df.iloc[i]["zscore"] <= -zscore_threshold or idf_df.iloc[i]["zscore"] >= zscore_threshold:
-                filter_set.add(idf_df.iloc[i]["token"])
-        return filter_set
+            if idf_df.iloc[i]["zscore"] <= -zscore or idf_df.iloc[i]["zscore"] >= zscore:
+                redundant_terms.add(idf_df.iloc[i]["token"])
+        return redundant_terms
 
     def _create_subplots(self, scores):
         """ Creates subplots corresponding to the histogram of scores. 
@@ -119,12 +112,10 @@ def filter_text(text, filter_set):
         text (list): tokenized text
         filterset (set): Set of filter words
     """
-
     if not isinstance(text, list):
         raise TypeError("Input must be a list.")
     if len(text) == 0:
         raise ValueError("Input must be a non empty list.")
-
     res = " ".join([t for t in text if t not in filter_set])
     return res
 
@@ -138,10 +129,10 @@ def save_filterset_tofile(filter_set, path):
 
 def create_filterset_map(p2_raw_train_df, output_dir, zs=[3]):
     original_df = pd.read_csv(p2_raw_train_df, sep="\t", names=["label", "text"])
-    wf = WordFilter()
+    wf = RedunWords()
     z_map = {}
     for z in zs:
-        filter_words = wf.idf_filterset(original_df.text, method="automatic", z=z)
+        filter_words = wf.get_redundant_terms(original_df.text, method="automatic", z=z)
         save_filterset_tofile(filter_words, os.path.join(output_dir, "z" + str(z) + "_filterset.txt"))
         z_map[z] = filter_words
     return z_map
@@ -149,8 +140,7 @@ def create_filterset_map(p2_raw_train_df, output_dir, zs=[3]):
 
 def create_filterset(p2_raw_train_df, output_dir):
     original_df = pd.read_csv(p2_raw_train_df, sep="\t", names=["label", "text"])
-    wf = WordFilter()
-
-    filter_words = wf.idf_filterset(original_df.text, method="manual", z=None)
+    wf = RedunWords()
+    filter_words = wf.get_redundant_terms(original_df.text, method="manual", z=None)
     save_filterset_tofile(filter_words, os.path.join(output_dir, "manual_filterset.txt"))
     return filter_words
